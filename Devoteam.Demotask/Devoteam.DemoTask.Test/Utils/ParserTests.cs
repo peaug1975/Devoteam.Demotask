a88b3ab135fbf42326eed9f91c4f1aa6f220ef86
Peter Augustsson <peter@papit.se>
2025-08-01 11:06:47 +0200

    Add input string parsing helper.

---
+++ Devoteam.Demotask/Devoteam.DemoTask.Test/Utils/ParserTests.cs
@@ -0,0 +1,65 @@
+﻿using Devoteam.Demotask.Exceptions;
+using Devoteam.Demotask.Model;
+using Devoteam.Demotask.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoteam.DemoTask.Test.Utils
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void RoomParserInputOkTest()
+        {
+            StringInputParser parser = new StringInputParser();
+            var room = parser.SquareRoomFromString("2 3");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("1")]
+        [InlineData("1 2 3")]
+        [InlineData("a 1")]
+        [InlineData("0 1")]
+        public void RoomParserInputNotOkTest(string notOkInput)
+        {
+            StringInputParser parser = new StringInputParser();
+            foreach (var notOk in notOkInput)
+            {
+                Assert.Throws<ParserException>(() => parser.SquareRoomFromString(notOkInput));
+            }
+        }
+
+        [Fact]
+        public void RobotParserInputOkTest()
+        {
+            StringInputParser parser = new StringInputParser();
+            var robot = parser.RobotFromString("0 1 N");
+            Assert.Equal(0, robot.X);
+            Assert.Equal(1, robot.Y);
+            Assert.Equal(Direction.North, robot.Dir);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("1")]
+        [InlineData("1 2")]
+        [InlineData("x 1 N")]
+        [InlineData("0 x N")]
+        [InlineData("-1 0 N")]
+        [InlineData("0 -1, N")]
+        [InlineData("1 1 X")]
+        public void RobotParserInputNotOkTest(string notOkInput)
+        {
+            var room = new SquareRoom(2, 2);
+            StringInputParser parser = new StringInputParser();
+            foreach (var notOk in notOkInput)
+            {
+                Assert.Throws<ParserException>(() => parser.RobotFromString(notOkInput));
+            }
+        }
+    }
+}
---
+++ Devoteam.Demotask/Devoteam.Demotask/Utils/StringInputParser.cs
@@ -0,0 +1,42 @@
+﻿using Devoteam.Demotask.Exceptions;
+using Devoteam.Demotask.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devoteam.Demotask.Utils
+{
+    public class StringInputParser
+    {
+        public SquareRoom SquareRoomFromString(string? input)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ParserException("Room input cannot be empty");
+            var inputs = input.Split(' ');
+            if (inputs.Length != 2) throw new ParserException("Room input must be two values separated by a space");
+            int width = -1;
+            if (!int.TryParse(inputs[0], out width)) throw new ParserException("Room width is not an integer");
+            if (width < 1) throw new ParserException("Room width must be grater than zero");
+            int depth = -1;
+            if (!int.TryParse(inputs[1], out depth)) throw new ParserException("Room depth is not an integer");
+            if (depth < 1) throw new ParserException("Room depth must be grater than zero");
+            return new SquareRoom(width, depth);
+        }
+        public Robot RobotFromString(string? input)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ParserException("Robot input cannot be empty");
+            var inputs = input.Split(' ');
+            if (inputs.Length != 3) throw new ParserException("Robot input must be three values separated by spaces");
+            int x = -1;
+            if (!int.TryParse(inputs[0], out x)) throw new ParserException("Robot x position is not an integer");
+            if (x < 0) throw new ParserException("Robot x position cannot be negative");
+            int y = -1;
+            if (!int.TryParse(inputs[1], out y)) throw new ParserException("Robot y position is not an integer");
+            if (x < 0) throw new ParserException("Robot y position cannot be negative");
+            Direction dir = EnumHelper.DirectionFromString(inputs[2]);
+            
+            return new Robot(x,y,dir);
+        }
+    }
+}
